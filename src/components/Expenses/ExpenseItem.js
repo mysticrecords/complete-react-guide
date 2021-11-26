e39016658b39f432150929320a61e9eb68cb1bdb
Matt Walsh <mysticrecords@hotmail.com>
2021-11-26 11:15:51 +0000

    complete-react-guide

---
+++ src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import ExpenseDate from "./ExpenseDate";
+import Card from "../UI/Card.js";
+
+import "./ExpenseItem.css";
+
+const ExpenseItem = (props) => {
+
+  const [title, setTitle] = useState(props.title);
+
+  const clickHandler = () => {
+    // setTimeout(() => {setTitle('Updated!')}, 2000);
+    setTitle('Updated!');
+    console.log(title);
+  };
+
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{title}</h2>
+      </div>
+      <div className="expense-item__price">£{props.amount}</div>
+      <button onClick={clickHandler}>Change button</button>
+    </Card>
+  );
+};
+
+export { ExpenseItem };
